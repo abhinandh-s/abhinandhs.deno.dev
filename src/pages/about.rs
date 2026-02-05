6e2bc9d04336378a3e8b43eb003124c70be7c0ba
abhi <abhinandhsuby@proton.me>
2026-02-05 15:20:30 +0530

    migration

--- src/pages/about.rs
+++ src/pages/about.rs
@@ -13,8 +13,9 @@ pub fn about_page() -> Html {
             class="p-2 mx-auto max-w-3xl flex flex-col justify-center">
 
                 <h1 class="border-l-4 border-l-just-red pl-4 font-bold max-tablet:text-3xl text-4xl mt-12">{ "Interests" }<span class="text-just-red">{"."}</span></h1>
-                <br />
-                <Link<Route> to={Route::Movies} classes="pt-8">{ "Movies" }</Link<Route>>
+                <div class="pt-8"></div>
+                <p>{ "some of my fav movies and series are listed down" }</p>
+                <Link<Route> to={Route::Movies} classes="text-just-red dark:text-just-red underline transition-colors duration-200">{ "Movies" }</Link<Route>>
     
                 <crate::components::footer::Footer />
             </div>
